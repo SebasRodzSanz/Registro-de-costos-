494bd91fa8be4fd7cc3780f20497d282e203864d
SebasRodzSanz <133052102+SebasRodzSanz@users.noreply.github.com>
2025-04-27 16:05:54 -0600

    Modificación en la vista departamento

--- vistas/vista_departamento.php
+++ vistas/vista_departamento.php
@@ -4,27 +4,10 @@ require "header.php"
   <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
     <!-- Content Header (Page header) -->
-    <section class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1>Legacy User Menu</h1>
-          </div>
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Legacy User Menu</li>
-            </ol>
-          </div>
-        </div>
-      </div><!-- /.container-fluid -->
-    </section>
-
     <!-- Main content -->
     <section class="content">
-
         <!-- Default box -->
-    <div class="card">
+      <div class="card">
         <div class="card-header">
             <h3 class="card-title">Title</h3>
         </div>
@@ -68,13 +51,8 @@ require "header.php"
             <!-- /.insert new department form -->
         </div>
         <!-- /.card-body -->
-        <div class="card-footer">
-          Footer
-        </div>
-        <!-- /.card-footer-->
-    </div>
+      </div>
     <!-- /.card -->
-
     </section>
     <!-- /.content -->
   </div>
